00f97c2e9197060820e9b6cae541ca61b408c395
LucasBrunengo <lucas.brunengo@alumni.esade.edu>
2024-10-09 14:28:03 +0200

    Update Python Class 2.py

--- Python Class 2.py
+++ Python Class 2.py
@@ -1,4 +1,16 @@
-#Excersice 1
+'''
+#Exercise 1: FizzBuzz
+
+1. Write a FizzBuzz Function: Create a function fizzbuzz(n) that takes an integer n as a parameter.
+
+2. Implement FizzBuzz Logic: The function should print:
+     * "Fizz" for multiples of 3
+     * "Buzz" for multiples of 5
+     * "FizzBuzz" for multiples of both 3 and 5
+     * The number itself for other numbers
+
+3. Call the Function: Call the function for numbers 1 to 20.
+'''
 
 def fizzbuzz(n) : 
     for i in range(1, n + 1) :
@@ -13,8 +25,15 @@ def fizzbuzz(n) :
 
 fizzbuzz(20)
 
+'''
+Exercise 2: Basic Data Filtering
 
-#Excersice 2
+1. Create a List of Mixed Data Types: Create a list that contains a mix of integers, strings, and floats.
+
+2. Filter the List: Use list comprehension to create a new list that contains only the integers from the original list.
+
+3. Print the New List: Output the filtered list of integers.
+'''
 
 Mixed_List = ["Lucas", 24, 24.7, "Agustin", 27, 27.9, "Manuel", 18, 18.1]
 
@@ -23,7 +42,15 @@ Int_List = [x for x in Mixed_List if isinstance(x, int)]
 print(Int_List)
 
 
-#Exercise 3
+'''
+Exercise 3: Simple To-Do List
+
+1. Create an Empty List: Start with an empty list called todo_list.
+
+2. Define Functions:
+    * A function add_task(task) that adds a task to the list.
+    * A function show_tasks() that prints all tasks in the list.
+'''
 
 todo_list = []
 
@@ -42,7 +69,13 @@ add_task("Buy milk")
 add_task("Call dad")
 show_tasks()
 
-#Excercise 4
+'''
+Exercise 4: Temperature Converter
+
+1. Define a Conversion Function: Write a function celsius_to_fahrenheit(celsius) that converts Celsius to Fahrenheit.
+
+2. Print the Result: Output the converted temperature for 22ºF, 46ºF, 51ºF and 76ºF.
+'''
 
 def celsius_to_fahrenheit(celsius) :
     return (celsius * 1.8) + 32
